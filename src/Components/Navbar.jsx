5aff86ccd3c2ee060f809b3a224ac2f47495f44c
shabry <aria.shabry@gmail.com>
2025-03-07 15:34:19 +0700

    fix: hire me button

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
                     >
                         {isDarkMode ? "🌞" : "🌙"}
                     </button> */}
-                    <button type="button" className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-10 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Hire Me</button>
+                    <a type="button" className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-10 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800" href="#contact">Hire Me</a>
 
                     <button
                         data-collapse-toggle="navbar-sticky"
